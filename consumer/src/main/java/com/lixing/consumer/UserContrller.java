313809857b903d9eef74aa464b3e6286b903856c
lixing <369431735@qq.com>
2017-09-15 11:36:46 +0800

    忘了改的啥

--- consumer/src/main/java/com/lixing/consumer/UserContrller.java
+++ consumer/src/main/java/com/lixing/consumer/UserContrller.java
@@ -8,15 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class UserContrller {
+public class UserContrller extends BaseController {
     @Autowired
   private RestTemplate restTemplate;
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id){
         return this.restTemplate.getForObject("http://localhost:8080/"+id,User.class);
     }
-    @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
+
 }
--- provider/src/main/java/com/lixing/provider/ProviderApplication.java
+++ provider/src/main/java/com/lixing/provider/ProviderApplication.java
@@ -2,8 +2,10 @@ package com.lixing.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient //也可以使用@EnableEurekaClient代替 在spring cloud中服务发现组件有多种选择，例如Zookeeper、consul等。@EnableDiscoveryClient为各种服务组件提供了支持
 public class ProviderApplication {
 
 	public static void main(String[] args) {
